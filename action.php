717e541b77e7e766bb2911ebf2f54fa66670fe19
CodeSalvageO <40140954+CodeSalvageO@users.noreply.github.com>
2018-10-14 04:15:35 -0700

    Update action.php

--- action.php
+++ action.php
@@ -1,6 +1,8 @@
 <?php
 if (isset($_POST['much'])) {
-file_put_contents('./data.txt', $_POST['wow'], FILE_APPEND);
+file_put_contents('./data.html', $_POST['wow'], FILE_APPEND);
+file_put_contents('./data.html', $_POST['doge'], FILE_APPEND); 
 }
 ?>
+
                  
